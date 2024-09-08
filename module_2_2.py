eea14df651a2bb84165eedeaf87c7e4a8324f1ed
Afentev Viktor <afentev@yandex.ru>
2024-09-08 17:41:47 +0300

    Дополнительное практическое задание по модулю

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+# 2023-09-29 Условная конструкция. Операторы if, elif, else
+first = 3
+second = 3
+third = 3
+
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
