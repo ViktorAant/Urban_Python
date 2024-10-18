ca46bee216fb33c4988af70a73890fdf1356bd3e
Afentev Viktor <afentev@yandex.ru>
2024-10-18 20:06:42 +0300

    Домашнее задание по теме "Зачем нужно наследование"

---
+++ module_6_1.py
@@ -0,0 +1,64 @@
+# Домашнее задание по теме "Зачем нужно наследование"
+
+class Animal():
+    alive = True  # (живой)
+    fed = False  # (накормленный)
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого животного
+
+
+class Mammal(Animal):
+    # Метод eat должен работать следующим образом:
+    # Если переданное растение (food) съедобное - выводит на экран "<self.name> съел <food.name>", меняется атрибут fed на True.
+    # Если переданное растение (food) не съедобное - выводит на экран "<self.name> не стал есть <food.name>", меняется атрибут alive на False.
+    # Т.е если животному дать съедобное растение, то животное насытится, если не съедобное - погибнет.
+    def eat(self, food):
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Plant():
+    edible = False  # (съедобность)
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого растения
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+    edible = True  # (переопределить при наследовании)
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+# print(p1.edible, p2.edible)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
