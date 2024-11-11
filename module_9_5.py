f17230c9953316b27194a81a8da526a0756b09ad
Afentev Viktor <afentev@yandex.ru>
2024-11-11 20:17:10 +0300

    Домашнее задание по теме "Итераторы"

--- module_9_5.py
+++ module_9_5.py
@@ -1,16 +1,55 @@
-class MyClass:
-    def __init__(self):
-        self.state = 0
+'''
+Итераторы
+Цель: освоить механизмы работы итераторов и описания методов __next__ и __iter__.
+Закрепить навык создания и выбрасывания исключений.
+'''
+
+
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start  # целое число, с которого начинается итерация.
+        self.stop = stop  # целое число, на котором заканчивается итерация.
+        self.step = step  # шаг, с которым совершается итерация.
+        self.pointer = start  # указывает на текущее число в итерации (изначально start)
 
     def __iter__(self):
+        self.pointer = self.start - self.step
         return self
 
     def __next__(self):
-        self.state += 1
-        if self.state > 4:
+        self.pointer += self.step
+        if (self.step < 0 and self.pointer < self.stop) or (self.step > 0 and self.pointer > self.stop):
             raise StopIteration
-        return self.state
+        return self.pointer
 
-myObj = MyClass()
-for i in myObj:
-    print(i)
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
