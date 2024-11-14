6152fdd9b31c25dbe8c7b995d9e21431183ae89f
Afentev Viktor <afentev@yandex.ru>
2024-11-14 18:53:30 +0300

    Домашнее задание по теме "Декораторы"

--- module_9_7.py
+++ module_9_7.py
@@ -6,23 +6,39 @@
 Практически применить знания, создав функцию декоратор и обернув ею другую функцию.
 '''
 
-Задание:
-Напишите 2 функции:
 
-    Функция, которая складывает 3 числа (sum_three)
-    Функция декоратор (is_prime), которая распечатывает "Простое",
-    если результат 1ой функции будет простым числом и "Составное" в противном случае.
+# Вариант 1 - с циклом "for"
+# def is_prime(func):
+#     def wrapper(*args):
+#         original_result = func(*args)
+#         prime_n = "Простое"
+#         for i in range(2, int(original_result / 2) + 1):
+#             if original_result % i == 0:
+#                 prime_n = "Составное"
+#                 break
+#         print(prime_n)
+#         return original_result
+#     return wrapper
 
-Пример:
+# Вариант 2 - с использованием исключений и сборок
+def is_prime(func):
+    def wrapper(*args):
+        original_result = func(*args)
+        try:
+            lst = [1 / 0 for i in range(2, int(original_result / 2) + 1) if original_result % i == 0]
+        except ZeroDivisionError:
+            print("Составное")
+        else:
+            print("Простое")
+        return original_result
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
 result = sum_three(2, 3, 6)
 print(result)
-
-Результат консоли:
-Простое
-11
-
-Примечания:
-
-    Не забудьте написать внутреннюю функцию wrapper в is_prime
-    Функция is_prime должна возвращать wrapper
-    @is_prime - декоратор для функции sum_three
