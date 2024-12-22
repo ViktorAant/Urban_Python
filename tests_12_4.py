b71568c04c58370723eb3ee5e74b586931413e49
Afentev Viktor <afentev@yandex.ru>
2024-12-22 12:35:39 +0300

    Домашнее задание по теме "Логирование"

---
+++ tests_12_4.py
@@ -0,0 +1,87 @@
+'''
+Логирование
+Цель: получить опыт использования простейшего логирования совместно с тестами
+'''
+import logging
+import unittest
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        try:
+            walker = Runner('somename', -1)
+            self.runner = Runner('somename')
+            for i in range(10):
+                self.runner.walk()
+            self.assertEqual(50, self.runner.distance)
+            logging.info("'test_walk' выполнен успешно")
+        except:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+    def test_run(self):
+        try:
+            runner = Runner({1, 2})
+            self.runner = Runner('somename')
+            for i in range(10):
+                self.runner.run()
+            self.assertEqual(100, self.runner.distance)
+            logging.info("'test_run' выполнен успешно")
+        except:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
+                        format='%(asctime)s | %(levelname)s | %(message)s')
+    unittest.main()
+
