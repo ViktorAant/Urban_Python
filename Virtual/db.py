fd824435ece4725d316d2fc08cd01824a88cf8cf
ViktorAfnt <afentev@yandex.ru>
2025-01-10 19:58:44 +0300

    Add files

---
+++ Virtual/db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
+
+connection.commit()
+connection.close()
