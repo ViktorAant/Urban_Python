9d29d240ce9b3cd1c831580aa83aedc8cf62413f
ViktorAfnt <afentev@yandex.ru>
2025-01-13 21:11:18 +0300

    Домашнее задание по теме "Написание примитивной ORM"

--- Aiogr/crud_functions.py
+++ Aiogr/crud_functions.py
@@ -28,14 +28,41 @@ def initiate_db():
     price INTEGER NOT NULL
     )
     ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL, 
+    email TEXT NOT NULL, 
+    age INT NOT NULL,
+    balance INT NOT NULL
+    );
+    ''')
     connection.commit()
 
 
 def fill_table():
     for i in range(1, 5):
         cursor.execute('INSERT INTO Products (title, description, price) VALUES(?, ?, ?)',
-                       (f'Product {i}', f'Описание {i}', f'{i}00'));
+                       (f'Product {i}', f'Описание {i}', f'{i}00'))
     connection.commit()
+
 
 def get_all_products():
     return cursor.execute('SELECT * FROM Products').fetchall()
+
+
+def is_included(username):
+    check_user = cursor.execute('''
+    SELECT * FROM Users WHERE username = ?
+    ''', (username,))
+    if check_user.fetchone():
+        return True
+    else:
+        return False
+
+
+def add_user(username, email, age):
+    if not is_included(username):
+        cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                       (username, email, age, 1000))
+        connection.commit()
---
+++ Aiogr/module_14_5.py
@@ -0,0 +1,178 @@
+'''
+План написания админ панели
+Цель: написать простейшие CRUD функции для взаимодействия с базой данных
+'''
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+import crud_functions as ff
+
+ff.initiate_db()
+all_products = ff.get_all_products()
+if not all_products:
+    ff.fill_table()
+    all_products = ff.get_all_products()
+
+bot = Bot(token=ff.API)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+calculate = KeyboardButton(text='Рассчитать')
+information = KeyboardButton(text='Информация')
+buy = KeyboardButton(text='Купить')
+reg = KeyboardButton(text='Регистрация')
+kb = ReplyKeyboardMarkup(resize_keyboard=True).add(calculate, buy, reg, information)
+
+ikb = InlineKeyboardMarkup(resize_keyboard=True)
+calculate = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+formulas = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+ikb.add(calculate, formulas)
+
+bkb = InlineKeyboardMarkup(resize_keyboard=True)
+# buy1 = InlineKeyboardButton(text='Product1', callback_data='product_buying')
+# buy2 = InlineKeyboardButton(text='Product2', callback_data='product_buying')
+# buy3 = InlineKeyboardButton(text='Product3', callback_data='product_buying')
+# buy4 = InlineKeyboardButton(text='Product4', callback_data='product_buying')
+# bkb.add(buy1, buy2, buy3, buy4)
+buy1 = InlineKeyboardButton(text='Оплатить', callback_data='product_buying')
+bkb.add(buy1)
+
+
+@dp.message_handler(commands=['start'])
+async def starter(message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=kb)
+
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for i, row in enumerate(all_products, start=1):
+        await message.answer_photo(photo=open(f'pics/{i}.png', 'rb'), \
+                                   caption=f'\rНазвание: {row[1]} | Описание: {row[2]} | Цена: {row[3]}\n', \
+                                   reply_markup=bkb)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer("Вы успешно приобрели продукт!")
+    await call.answer()
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer("Выберите опцию:", reply_markup=ikb)
+
+
+@dp.callback_query_handler(text='formulas')
+async def set_age(call):
+    await call.message.answer(
+        "Упрощенный вариант формулы Миффлина-Сан Жеора:\n \
+        для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;\n \
+        для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161")
+    await call.answer()
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+    gender = State()
+
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+
+
+@dp.message_handler(text='Регистрация')
+async def sing_up(message):
+    await message.answer("Введите имя пользователя (только латинский алфавит):")
+    await RegistrationState.username.set()
+
+
+@dp.message_handler(state=RegistrationState.username)
+async def set_username(message, state):
+    if ff.is_included(message.text):
+        await message.reply(f'Пользователь {message.text} существует, введите другое имя', reply=False)
+        return
+    await state.update_data(username=message.text)
+    data = await state.get_data()
+    await message.answer("Введите свой email:")
+    await RegistrationState.email.set()
+
+
+@dp.message_handler(state=RegistrationState.email)
+async def set_age(message, state):
+    await state.update_data(email=message.text)
+    data = await state.get_data()
+    await message.answer("Введите свой возраст:")
+    await RegistrationState.age.set()
+
+    @dp.message_handler(state=RegistrationState.age)
+    async def set_age(message, state):
+        await state.update_data(age=message.text)
+        data = await state.get_data()
+        ff.add_user(data['username'], data['email'], data['age'])
+        await message.reply('Регистрация прошла успешно', reply=False)
+        await state.finish()
+
+
+# -----------------------------------------------------------
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer("Введите свой возраст:")
+    await UserState.age.set()
+    await call.answer()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    data = await state.get_data()
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_gender(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer("Признайтесь, корнет, вы - женщина? (Y/N):")
+    await UserState.gender.set()
+
+
+@dp.message_handler(state=UserState.gender)
+async def send_calories(message, state):
+    await state.update_data(gender=message.text)
+    data = await state.get_data()
+    if data['gender'] == 'Y':
+        await message.answer(
+            f"Упрощенный вариант формулы Миффлина-Сан Жеора для женщин. \
+            {(10 * float(data['weight'])) + (6.25 * float(data['growth'])) - (5 * float(data['age'])) - float(161)}")
+    else:
+        await message.answer(
+            f"Упрощенный вариант формулы Миффлина-Сан Жеора для мужиков. \
+            {(10 * float(data['weight'])) + (6.25 * float(data['growth'])) - (5 * float(data['age'])) + float(5)}")
+    await state.finish()
+
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer("Информация о боте.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
